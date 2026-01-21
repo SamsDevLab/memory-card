0caa8b3128109d54c76ce7f19900c869913b1f31
Sam Hadfield <sthtech@protonmail.com>
2026-01-21 14:39:47 -0600

    feat: add data objs to be used for cards
    Data objs in the charactersArr will be used to propagate cards

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const charactersArr = [
+  {
+    id: crypto.randomUUID(),
+    name: "Stanley Ipkiss",
+    bio: "Shy, unlucky, and mild-mannered bank clerk who transforms into a chaotic, green-faced, cartoonish trickster when he puts on a magical mask, gaining incredible powers to wreak havoc, exact revenge, and live out his wildest, most unrestrained desires.",
+    image: "",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Charlie Baileygates aka Hank Evans",
+    bio: "Mild-mannered Rhode Island State Trooper who develops a violent, aggressive split personality named Hank Evans (Henry Evans) to cope with stress, leading him to commit various crimes and cause chaos while unknowingly being controlled by Hank, a chaotic alter-ego with a desire to hurt people and manipulate women",
+    image: "",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Ace Ventura, Pet Detective",
+    bio: "Flamboyant, eccentric, Miami-based private detective specializing in the retrieval of missing animals",
+    image: "",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Edward Nygma aka The Riddler",
+    bio: "Eccentric, rejected Wayne Enterprises scientist obsessed with Bruce Wayne, transforming into a flamboyant villain who uses mind-bending puzzles and his 'Box' invention to force Batman to reveal his identity and prove his intellectual superiority",
+    image: "",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Chip Douglas aka The Cable Guy",
+    bio: "Mentally unstable cable installer who gets obssessed with one of his customers, Steven Kovacs. After his numerous friendship advances get turned down by Steven, Chip ends up becoming a fully-fledged stalker, hellbent on destroying Steven's life",
+    image: "",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Lloyd Christmas",
+    bio: "Dim-witted, immature, but good-hearted limo driver who embarks on chaotic cross-country adventures with his best friend Harry Dunne. Known for his signature chipped tooth and romantic infatuation with Mary Swanson. ",
+    image: "",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Fletcher Reede",
+    bio: "Fast-talking, habitually lying defense attorney whose career built on deceit is upended when his son Max wishes for him to be unable to lie for 24 hours. His inability to lie throws his successful, but morally compromised, career into chaos and highlights his failures as a father",
+    image: "",
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "The Grinch",
+    bio: "An eccentric, short-tempered, misanthropic, cynical and scheming green-furred creature, who despises Christmas",
+    image: "",
+  },
+  {
+    id: "crypto.randomUUID()",
+    name: "Fire Marshal Bill",
+    bio: "Popular, clumsy, and accident-prone fire safety inspector. Enthusiastically inspects homes for fire hazards, often causing more destruction with his well-meaning but disastrous attempts to demonstrate fire safety, much to the horror of homeowners.",
+    image: "",
+  },
+  {
+    id: "crypto.randomUUID()",
+    name: "Truman Burbank",
+    bio: "Insurance salesman whose entire life, from birth, has been a massive reality TV show, broadcast globally without his knowledge, featuring paid actors as his family and friends in a fabricated island town called Seahaven. He gradually uncovers the elaborate deception and attempts to escape his artificial world",
+    image: "",
+  },
+];
+
+export default charactersArr;
