b17f402d342aa3ef1a3ceefe45efa89b9870ad29
Sam Hadfield <sthtech@protonmail.com>
2026-01-21 16:44:37 -0600

    feat: pass in character data, mv h1 from App.css
    Pass character data to Gameboard via charactersArr prop. Mv h1 styling
    from App.css to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
+import charactersArr from "./data";
 import Scoreboard from "./components/Scoreboard";
 import Gameboard from "./components/Gameboard";
+
+// console.log(charactersArr);
 
 function App() {
   return (
     <main className="main-container">
-      <h1>Jim Carrey Card Selector </h1>
+      <h1>Jim Carrey Memory Game </h1>
       <Scoreboard />
-      <Gameboard />
+      <Gameboard charactersArr={charactersArr} />
     </main>
   );
 }
@@ -16,34 +19,38 @@ export default App;
 
 /* 
 App Design Ideas:
-Memory Card Project
-
-Jim Carrey API characters
-
-1. The Mask
-2. The Cable Guy
-3. Riddler
-4. Dumb and Dumber
-5. Me, Myself, and Irene
-6. Ace Ventura
-7. Liar, Liar
-8. The Grinch
-9. Fire Marshall Bill
-10. Man on the Moon
 
 Components Needed
 
-Scoreboard
-  • CurrentScore component counter
-  • BestScore component - updates with best score after a loss
+After lunch: make a data.js file and start playing around with the objects structures and
+delivering that data to the Gameboard
+
+Scoreboard:
+  • Possibly need a counter on card objects
+    • When a card object is clicked, the counter increases to 1
+    • If a counter goes above 1, this means the card has been clicked twice
+    • If that happens, the player loses the game
+
+  GameBoard:
+  • Holds cards in component - organizes them via grid
+  • Click Fn 
+    • When clicking a card, cards will randomize
+    • This will probably need to pass down as a prop to “Cards” component
+  • Cards component
+    • GameBoard will need to implement useEffect() to re-call the API after click
 
 
-  GameBoard
-      /*
-      • Holds cards in component - organizes them via grid
-      • Click Fn 
-        • When clicking a card, cards will randomize
-        • This will probably need to pass down as a prop to “Cards” component
-      • Cards component
-      • GameBoard will need to implement useEffect() to re-call the API after click
+  Jim Carrey API characters
+
+  ✅ 1. The Mask
+  ✅ 2. The Cable Guy
+  ✅ 3. Riddler
+  ✅ 4. Dumb and Dumber
+  ✅ 5. Me, Myself, and Irene
+  ✅ 6. Ace Ventura
+  ✅ 7. Liar, Liar
+  ✅ 8. The Grinch
+  ✅ 9. Fire Marshall Bill
+  ✅ 10. The Truman Show
+
 */
