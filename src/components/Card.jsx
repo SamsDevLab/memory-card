45bb03658ece372827ed12d306c5848fa00d7497
Sam Hadfield <sthtech@protonmail.com>
2026-01-27 12:25:36 -0600

    feat: add characterProfile props to img, size img
    Add characterProfile to img and alt tags with Card component. Set
    min/max heights on cards within gameboard.css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 function Card({ characterProfile, onClick }) {
   return (
     <div className="card" onClick={() => onClick(characterProfile)}>
-      {/* <img src="" alt="" /> */}
+      <img src={characterProfile.image} alt={characterProfile.name} />
       <h2 className="character-header">{characterProfile.name}</h2>
       <p>{characterProfile.bio}</p>
     </div>
