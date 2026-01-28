1d78826380c1fc2c546578c6bb1a12a80bdaf0d8
Sam Hadfield <sthtech@protonmail.com>
2026-01-28 12:05:55 -0600

    Add bestScore to Scoreboard component

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,10 +1,10 @@
 import "../stylesheets/scoreboard.css";
 
-function Scoreboard({ scoreCounter }) {
+function Scoreboard({ score, bestScore }) {
   return (
     <section className="scoreboard">
-      <h2>Score: {scoreCounter}</h2>
-      <h2>Best Score: </h2>
+      <h2>Score: {score}</h2>
+      <h2>High Score: {bestScore} </h2>
     </section>
   );
 }
